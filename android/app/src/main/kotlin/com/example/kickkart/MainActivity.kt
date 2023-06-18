f52b6e978c9085213025269075019ff89e9c25f6
Yash Londhe <yashlondhe9151@gmail.com>
2023-06-18 23:19:10 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/kickkart/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.kickkart
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/auth/auth.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class AuthPage extends StatefulWidget {
+  const AuthPage({super.key});
+
+  @override
+  State<AuthPage> createState() => _AuthPageState();
+}
+
+class _AuthPageState extends State<AuthPage> {
+  bool a = true;
+  void go() {
+    setState(() {
+      a = !a;
+
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {(
+    if (a) {
+       return LoginPage(show: go);
+
+    } else {
+      return SignUp(show: go);
+
+    }
+    
+  }
+}
