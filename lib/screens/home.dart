16dd24c90275a404e8d6a2e16a5e7cdf8c7f4c87
Yash Londhe <yashlondhe9151@gmail.com>
2023-07-03 19:00:08 +0530

    Update: home screen page and added the banners

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -9,6 +9,8 @@ class Home extends StatefulWidget {
   State<Home> createState() => _HomeState();
 }
 
+final _control = PageController();
+
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
@@ -17,34 +19,114 @@ class _HomeState extends State<Home> {
       body: SafeArea(
         child: CustomScrollView(
           slivers: [
+            AppBarr(),
+            SearchBox(),
             SliverToBoxAdapter(
-              child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+              child: Column(
+                children: [
+                  Container(
+                    height: 200,
+                    width: double.infinity,
+                    child: PageView(
+                      controller: _control,
+                      scrollDirection: Axis.horizontal,
+                      children: [
+                        Container(color: Colors.amber),
+                        Container(color: Colors.amber),
+                        Container(color: Colors.amber),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  SliverToBoxAdapter SearchBox() {
+    return SliverToBoxAdapter(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 5),
+        child: Row(
+          children: [
+            Expanded(
+              child: Container(
+                height: 50,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.white,
+                ),
                 child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    Container(
-                      width: 40,
-                      height: 40,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Colors.white,
-                      ),
-                      child: Icon(Icons.list),
+                    SizedBox(
+                      width: 15,
                     ),
-                    Container(
-                      width: 40,
-                      height: 40,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        image: DecorationImage(image: AssetImage('images/p3.jpg'))
-                      ),
+                    Icon(Icons.search, color: Colors.grey),
+                    SizedBox(
+                      width: 15,
                     ),
+                    Text('Search your shoes',
+                        style: TextStyle(
+                          color: Color.fromARGB(255, 194, 187, 187),
+                          fontWeight: FontWeight.w600,
+                        )),
                   ],
                 ),
               ),
-            )
+              flex: 6,
+            ),
+            SizedBox(width: 10),
+            Expanded(
+              child: Container(
+                height: 50,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: mains,
+                ),
+                child: Icon(
+                  Icons.filter_alt,
+                  color: Colors.white,
+                  size: 27,
+                ),
+              ),
+              flex: 1,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  SliverToBoxAdapter AppBarr() {
+    return SliverToBoxAdapter(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              width: 40,
+              height: 40,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.white,
+              ),
+              child: Icon(Icons.list),
+            ),
+            Container(
+              width: 40,
+              height: 40,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                image: DecorationImage(
+                  image: AssetImage('images/p3.jpg'),
+                ),
+              ),
+            ),
           ],
         ),
       ),
